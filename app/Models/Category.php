4d0ddf47c920ab7039918b0b450672351912c6c0
ikramasifkhan <asifkhanradi@gmail.com>
2020-04-15 20:08:55 +0600

    category table foreign key bug fixed, CategoryTableSeeder added

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -3,10 +3,22 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Support\Str;
 class Category extends Model
 {
     protected $guarded=[];
+
+    /**
+     * The "booted" method of the model.
+     *
+     * @return void
+     */
+    protected static function booted()
+    {
+        static::creating(function ($category) {
+            $category->slug = Str::slug($category->name);
+        });
+    }
 
     public function child_category(){
         return $this->hasMany(__CLASS__);
