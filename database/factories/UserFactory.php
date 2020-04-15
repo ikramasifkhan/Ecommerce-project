da66d715cb08f1f0e115227ffb6908f13887fc61
ikramasifkhan <asifkhanradi@gmail.com>
2020-04-15 09:47:14 +0600

    UserFactory added

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,10 +2,10 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
-use App\User;
 use Faker\Generator as Faker;
 use Illuminate\Support\Str;
-
+use App\Models\User;
+use Carbon\Carbon;
 /*
 |--------------------------------------------------------------------------
 | Model Factories
@@ -21,8 +21,8 @@ $factory->define(User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
-        'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-        'remember_token' => Str::random(10),
+        'phone_number'=>$faker->unique()->phoneNumber,
+        'password'=>bcrypt(123456),
+        'email_verified_at' => Carbon::now()
     ];
 });
