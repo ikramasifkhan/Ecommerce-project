e729c0ad66ab1d434d4fed6cd3403ffa1d70216a
ikramasifkhan <asifkhanradi@gmail.com>
2020-04-16 08:24:36 +0600

    ProductTabaleSeeder added

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,16 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\Product;
+use Faker\Generator as Faker;
+use App\Models\Category;
+
+$factory->define(Product::class, function (Faker $faker) {
+    return [
+        'category_id'=>Category::all()->random()->id,
+        'name'=>$faker->unique()->name,
+        'description'=>$faker->realText(),
+        'price'=>random_int(100, 1000),
+    ];
+});
---
+++ database/seeds/ProductTabaleSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Models\Product;
+use Illuminate\Database\Seeder;
+
+class ProductTabaleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(Product::class, 10)->create();
+        $products = Product ::select('id')->get();
+
+        $url = 'https://lorempixel.com/640/480/?30114';
+        foreach($products as $product){
+            $product->addMediaFromUrl($url)->toMediaCollection('products');
+        }
+    }
+}
