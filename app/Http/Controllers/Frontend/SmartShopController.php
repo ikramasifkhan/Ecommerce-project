ca0974192c3105240c98298fec6dc73a2a07fc76
ikramasifkhan <asifkhanradi@gmail.com>
2020-04-19 09:01:52 +0600

    SmartShopController added

--- app/Http/Controllers/Frontend/SmartShopController.php
+++ app/Http/Controllers/Frontend/SmartShopController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class SmartShopController extends Controller
 {
     public function index(){
-        return view('frontend.home.home');
+       $products = Product::select(['id', 'name', 'slug', 'price'])
+            ->where('publication_status', 1)
+            ->paginate(9);
+        return view('frontend.home.home', ['products'=>$products]);
     }
 }
