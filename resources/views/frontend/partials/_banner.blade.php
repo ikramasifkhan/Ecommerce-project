304d9ccbcd099a405a8824a894e0265d36799acb
ikramasifkhan <asifkhanradi@gmail.com>
2020-04-19 09:02:56 +0600

    banner section added on partials folder

---
+++ resources/views/frontend/partials/_banner.blade.php
@@ -0,0 +1,10 @@
+<section class="jumbotron text-center">
+    <div class="container">
+        <h1>Smart shop</h1>
+        <p class="lead text-muted">Buy anyting from our store</p>
+        <p>
+            <a href="#" class="btn btn-primary my-2">Main call to action</a>
+            <a href="#" class="btn btn-secondary my-2">Secondary action</a>
+        </p>
+    </div>
+</section>
