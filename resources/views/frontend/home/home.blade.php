4c72f505d0b8edbfae02f9af58072bc8029091dd
ikramasifkhan <asifkhanradi@gmail.com>
2020-04-21 10:09:37 +0600

    product list added to the home page

--- resources/views/frontend/home/home.blade.php
+++ resources/views/frontend/home/home.blade.php
@@ -9,9 +9,11 @@
           @foreach ($products as $product)
                 <div class="col-md-4">
                     <div class="card mb-4 shadow-sm">
-                        <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+                        <a href="{{route('product.details', $product->slug)}}">
+                        <img src="{{$product->getFirstMedia('products')}}" alt="{{$product->name}}">
+                        </a>
                         <div class="card-body">
-                            <p class="card-text"><a href="" class="text-dark" style="text-decoration: none">{{$product->name}}</a></p>
+                            <p class="card-text"><a href="{{route('product.details', $product->slug)}}" class="text-dark" style="text-decoration: none">{{$product->name}}</a></p>
                             <div class="d-flex justify-content-between align-items-center">
                                 <div class="btn-group">
                                     <button type="button" class="btn btn-sm btn-outline-secondary">Add to cart</button>
